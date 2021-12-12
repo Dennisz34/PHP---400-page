1176752dabde14f7212c73353535e588b25907b2
Dennis Uku <84576822+Dennisz34@users.noreply.github.com>
2021-12-12 23:14:25 +0300

    404 not page
    Advanced 404 error page Cyberpunk

---
+++ PHP - 400 page/hata/Hata.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Cyberpunk Error page</title>
+  <meta name="theme-color" content="#101119">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<section class="error-body">
+	<video preload="auto" class="background" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/396624/err.mp4" autoplay muted loop></video>
+	<div class="message">
+		<h1 t="404">404</h1>
+		<div class="bottom">
+			<p>Sayfa Bulunamadı!</p>
+			<a href="#">Ana sayfaya dön</a>
+		</div>
+	</div>
+</section>
+  <script  src="./script.js"></script>
+</body>
+</html>
+
+
+
